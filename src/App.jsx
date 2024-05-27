63809410c2f7c5f08eff8ec143345c9300bd1c8e
kybaq <flfsk12@gmail.com>
2024-05-27 19:41:22 +0900

    ex: context 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import TextInput from "./components/TextInput";
 import TextList from "./components/TextList";
+import { AppContext } from "./context/AppContext";
 
 function App() {
   // TODO: texts 를 context api 로 리팩터링 하세요.
   const [texts, setTexts] = useState(() =>
     localStorage.getItem("texts")
       ? JSON.parse(localStorage.getItem("texts"))
-      : [],
+      : []
   );
 
   useEffect(() => {
@@ -19,11 +20,11 @@ function App() {
   };
 
   return (
-    <div>
+    <AppContext.Provider value={{ texts, setTexts }}>
       <h1>Text Input and Listing</h1>
       <TextInput onAddText={onAddText} />
-      <TextList texts={texts} />
-    </div>
+      <TextList />
+    </AppContext.Provider>
   );
 }
 
--- src/components/TextList.jsx
+++ src/components/TextList.jsx
@@ -1,4 +1,11 @@
-export default function TextList({ texts }) {
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+export default function TextList() {
+  const data = useContext(AppContext);
+
+  const { texts } = data;
+
   return (
     <ul>
       {texts.map((text, index) => (
